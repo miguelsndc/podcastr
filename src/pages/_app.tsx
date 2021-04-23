d78af88ea63fb5a5015f9424dc269e135ebb4375
Miguel Nogueira <mglsndc@gmail.com>
2021-04-23 15:14:34 -0300

    wrap player context logic on its own component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,28 @@
-import '../styles/global.scss';
+import Head from 'next/head';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerProvider } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
-import { PlayerProvider } from '../contexts/PlayerContext';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlayerProvider>
-      <div className={styles.appWrapper}>
-        <main>
-          <Header />
-          <Component {...pageProps} />
-        </main>
-        <Player />
-      </div>
-    </PlayerProvider>
+    <>
+      <Head>
+        <title>Podcastr</title>
+      </Head>
+      <PlayerProvider>
+        <div className={styles.appWrapper}>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+          <Player />
+        </div>
+      </PlayerProvider>
+    </>
   );
 }
 
